210b165b860f5990e2e019447a24823ece07e83a
Henry Tsai <henry88002605@gmail.com>
2020-04-18 18:16:40 +0800

    using a bootstrap template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 import "bootstrap";
 import "../stylesheets/application"
 
+
 document.addEventListener("turbolinks:load", ()=> {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
